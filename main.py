b2df52d6233bc2a8572cd1fa1bb16573ebbd1ed3
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2021-09-14 23:27:00 +0530

    modify: remove alias for pprint

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from gLearn import getClasses
 from getpass import getpass
-from pprint import pprint as pp
+from pprint import pprint
 
 pinNumber = input("Enter PIN Number: ")
 password = getpass("Enter Password: ")
 
 classesList = getClasses(pinNumber, password)
 
-pp(classesList)
+pprint(classesList)
